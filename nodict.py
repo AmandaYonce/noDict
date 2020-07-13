d15ada44baed023ceda95e899b35fa10578b1357
Amanda Yonce <amanda.stork@gmail.com>
2020-07-13 10:12:42 -0400

    finished program, passed all tests and corrected all flake8

--- nodict.py
+++ nodict.py
@@ -3,47 +3,80 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = '???'
+__author__ = 'amanda Yoncce'
 
 
-class Node:
+class Node():
     def __init__(self, key, value=None):
-        self.hash = None
-        self.key = None
-        self.value = None
-        # Your code here
-        return
+        self.key = key
+        self.hash = hash(self.key)
+        self.value = value
 
     def __repr__(self):
-        # Your code here
-        return
+        """
+        Prints a human readable representation of its
+        key/value contents when asked.
+        """
+        return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
-        # Your code here
-        return
+        """
+        allows the Node class to compare itself to other Node
+        Objects using the built-in == operator
+        """
+        if self.key == other.key:
+            return True
+        else:
+            return False
 
 
 class NoDict:
+    """A program that mimics the behavior of a python dictionary"""
+
     def __init__(self, num_buckets=10):
-        self.buckets = None
-        # Your code here
+        """Initializes number of buckets, default is 10"""
+        self.buckets = [[] for i in range(num_buckets)]
+        self.size = num_buckets
 
     def __repr__(self):
-        # Your code here
-        return
+        """Return a string version of the NoDict contents"""
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i,
+                         bucket in enumerate(self.buckets)])
 
-    def add(self, key, value):
-        # Your code here
-        return
+    def add(self, key, value=None):
+        """
+        Creates a new entry in our dictionary and stores
+        the entry in a bucket.
+        """
+        new_node = Node(key, value)
+        bucket = self.buckets[new_node.hash % self.size]
+        for key_value in bucket:
+            if key_value == new_node:
+                bucket.remove(key_value)
+                break
+        bucket.append(new_node)
 
     def get(self, key):
-        # Your code here
-        return
+        """
+        Accesses key value pairs.
+        """
+        key_val = Node(key)
+        bucket = self.buckets[key_val.hash % self.size]
+        for each in bucket:
+            if each == key_val:
+                return each.value
+        raise KeyError(f'{key} was not found')
 
     def __getitem__(self, key):
-        # Your code here
-        return
+        """
+        mimics the use of square bracket notation to
+        look up dictionary entries
+        """
+        return self.get(key)
 
     def __setitem__(self, key, value):
-        # Your code here
-        return
+        """
+        mimics the use of square bracket notation to
+        set dictionary entries
+        """
+        self.add(key, value)
